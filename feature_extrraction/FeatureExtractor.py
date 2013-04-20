2e21ff814422c4f53a1ead717322102e7fc9677c
Mark Lubin <melubin@gmail.com>
2013-04-20 09:54:20 -0700

    FE abstract class

---
+++ feature_extrraction/FeatureExtractor.py
@@ -0,0 +1,12 @@
+"""
+FeatureExtractor.py
+abstract class(interface) for feature extraction
+"""
+
+class FeatureExtractor:
+
+	"""
+	perhaps only method? training example -> vector
+	"""
+	
+	def extract(data): pass
