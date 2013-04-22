e1c7e5c1a5c0909957c2138a0663fdc86f9b59c1
Mark Lubin <melubin@gmail.com>
2013-04-22 15:18:26 -0700

    cost function implemented and tested

--- neural_network/NeuralNetwork.py
+++ neural_network/NeuralNetwork.py
@@ -13,7 +13,7 @@ class NeuralNetwork:
 	description : what is this NN? eg. English V. Chinese Classifier
 	nUnits      : size of hidden layer
 	nClasses    : number of output classes
-	theta       : list of theta vectors eg. theta[0] is theta for input->hidden layer transformation
+	theta       : list of theta matrices eg. theta[0] is theta for input->hidden layer transformation
 	"""
 
 	def __init__(self,activationFn,nUnits,nClasses,inputSize,nFeatures,description,theta=None):
@@ -62,12 +62,35 @@ class NeuralNetwork:
 	def getAccuracy(self,X,y):
 		return sum(map(lambda x: 1 if x[0] == x[1] else 0,zip(self.predict(X),y))) / float(len(y))
 
+	"""
+	convert vector like [0 1 2] to [[1 0 0]
+									[0 1 0]
+									[0 0 1]]
+	y : original vector
+	N : number of classes
+	"""
+	def matrix_rep(self,y,N):
+		I = np.identity(N)
+		return np.array([I[i].tolist() for i in y])
 
 
 	"""
 	return gradient for this matrix 
 	"""
-	def cost_function(self,X,y,theta):pass
+	def cost_function(self,X,y,theta,lmbd=1.):
+		Y  = self.matrix_rep(y,self.nClasses)
+		H = self.feedforward(X,theta)
+		m,n = H.shape
+
+		#compute cost
+		J = (-1 * Y) * np.log(H) - (np.ones(Y.shape) - Y)\
+			* np.log(np.ones(H.shape) - H)
+		J = 1./m * np.sum(np.sum(J))
+		
+		#compute regularization term
+		R =  lmbd/(2.0 * m) * sum([sum([q**2 for q in np.nditer(layer)]) for layer in theta])
+
+		return J + R
 
 	"""
 	compute gradient at this theta
@@ -92,10 +115,13 @@ def sigmoid(X):
 def main():
 	data = loadmat('ex3data1.mat')
 	X = data['X']
+
 	y = data['y'] - 1
+	y = np.array([i[0] for i in y])
 	data = loadmat('ex3weights.mat')
 	theta = [data['Theta1'],data['Theta2']]
 	N = NeuralNetwork(sigmoid,25,10,X.shape[0],X.shape[1],"ex3test",theta)
 	print "Test data accurate to %f" % N.getAccuracy(X,y)
+	print "Cost a test Theta %f" % N.cost_function(X,y,theta)
 
 if __name__ == "__main__": main()
