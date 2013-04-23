cc0f4e26d21ff9325dbc5ed5a174f633206dd9c6
Mark Lubin <melubin@gmail.com>
2013-04-23 12:46:17 -0700

    cleaned up code and finished generalized NN

--- neural_network/NeuralNetwork.py
+++ neural_network/NeuralNetwork.py
@@ -3,32 +3,32 @@
 	Single Hidden Layer(for now) MLP Neural Network
 	Mark Lubin
 """
-
+EPSILON = .12
 import numpy as np
-from scipy.io import loadmat
+from scipy.io import loadmat,savemat
 from scipy.optimize import fmin_tnc as minimizer
 
 class NeuralNetwork:
 	"""
 	activationFn: function eg. g = sigmoid(x)
 	description : what is this NN? eg. English V. Chinese Classifier
-	nUnits      : size of hidden layer
-	nClasses    : number of output classes
+	params		: list of matrix sizes for the thetas
 	theta       : list of theta matrices eg. theta[0] is theta for input->hidden layer transformation
+				  used with saved matrices only
 	"""
 
-	def __init__(self,activationFn,activationFnGrad,nUnits,nClasses,inputSize,nFeatures,description,theta=None):
+	def __init__(self,activationFn,activationFnGrad,params,description,theta=None):
 		self.activationFn = activationFn
 		self.activationFnGrad = activationFnGrad
 		self.description = description
-		#self.nLayers = nLayers TODO variable number of hidden layers
-		self.nUnits = nUnits
-		self.nClasses = nClasses
-		self.nFeatures = nFeatures
+		self.theta = []		
 		if theta:
 			self.theta = theta
 		else:
-			self.theta = [.12 * np.random.rand(nUnits,nFeatures+1), .12 * np.random.rand(nClasses,nUnits+1)]
+			for dim in params:
+				self.theta.append(EPSILON * np.random.random_sample(dim))
+			#import pdb;pdb.set_trace();
+			#self.theta = [.12 * np.random.rand(nUnits,nFeatures+1), .12 * np.random.rand(nClasses,nUnits+1)]
 
 	"""
 	train neural network on provided dataset
@@ -36,7 +36,8 @@ class NeuralNetwork:
 	def train(self,X,y):
 		#import pdb;pdb.set_trace();
 		theta0 = self.roll(self.theta)
-		results = minimizer(lambda x: self.cost_function(X,y,x),theta0,lambda x: self.gradient(X,y,x))
+		results = minimizer(lambda x: self.cost_function(X,y,x),theta0,approx_grad = False)
+		#,lambda x: self.gradient(X,y,x))
 		self.theta = self.unroll(self.theta,results[0])
 
 	"""
@@ -44,19 +45,20 @@ class NeuralNetwork:
 	"""
 	def feedforward(self,X,theta):
 		nExamples, nFeatures = X.shape
-		if(nFeatures != self.nFeatures):
-			raise Exception("Invalid number of features got %d expected %d." %\
-			 (nFeatures, self.nFeatures))
-		A = [np.concatenate((np.ones((X.shape[0],1)),X),1)]
+		X = np.concatenate((np.ones((X.shape[0],1)),X),1)
+		A = [X[:]]
 		Z = [None] #spacer because there is no Z[0]
 		
 		#compute feed forward propagation
 		for W in theta:
-			X = np.concatenate((np.ones((X.shape[0],1)),X),1) #add column of ones
+			 #add column of ones
 			X = X.dot(W.T)
 			Z.append(X[:])
 			X = self.activationFn(X)
+			X = np.concatenate((np.ones((X.shape[0],1)),X),1)
 			A.append(X[:])
+
+		A[-1] = A[-1][:,1:] #remove 1's from output layer
 
 		return A,Z
 
@@ -65,7 +67,7 @@ class NeuralNetwork:
 	"""
 	def predict(self,X,theta=None):
 		if not theta: theta = self.theta
-		A,Z = self.feedforward2(X,theta)
+		A,Z = self.feedforward(X,theta)
 		return np.argmax(A[-1],1)
 	
 	"""
@@ -106,13 +108,14 @@ class NeuralNetwork:
 
 
 	"""
-	return gradient for this matrix 
+	return cost & gradient for this theta
 	"""
 	def cost_function(self,X,y,flat_theta,lmbd=1.):
 		#import pdb;pdb.set_trace();
 		theta = self.unroll(self.theta,flat_theta)
-		Y  = self.matrix_rep(y,self.nClasses)
-		H = self.feedforward(X,theta)
+		Y  = self.matrix_rep(y,self.theta[-1].shape[0])
+		A,Z = self.feedforward(X,theta)
+		H = A[-1]
 		m,n = H.shape
 		
 
@@ -125,44 +128,28 @@ class NeuralNetwork:
 		R =  lmbd/(2.0 * m) * sum([sum([q**2 for q in np.nditer(layer)]) for layer in theta])
 
 		print "Cost: %f" % (J+R)
-		return J + R
+
+		G = self.gradient(A,Z,Y,(m,n),theta)
+		#G1 = self.gradient(X,y,flat_theta)
+		#import pdb;pdb.set_trace();
+		return (J + R),G
 
 
-	def gradient(self,X,y,flat_theta):
-		m,n = X.shape
-		theta = self.unroll(self.theta,flat_theta)
-		Y = self.matrix_rep(y,self.nClasses)
-
-		delta1 = np.zeros(theta[0].shape)
-		delta2 = np.zeros(theta[1].shape)
-
-		for i,row in enumerate(X):
-			a1 = np.concatenate(([1],row))
-			z2 = a1.dot(theta[0].T)
-			a2 = np.concatenate(([1],self.activationFn(z2)))
-			z3 = a2.dot(theta[1].T)
-			a3 = self.activationFn(z3)
-
-			yi = Y[i]
-
-			d3 = a3 - yi
-
-			d2 = d3.dot(theta[1]) * np.concatenate(([1],self.activationFnGrad(z2)))
-			d2 = d2[1:]
-			d3 = np.reshape(d3,(d3.shape[0],1))
-			a2 = np.reshape(a2,(1,a2.shape[0]))
+	"""
+	calculate gradient using vectored, back propagation, variable layers
+	"""
+	def gradient(self,A,Z,Y,shape,theta):
+		m,n = shape
+		D = [None]  * len(theta)
+		D[-1] = (A[-1] - Y)# for the output layer
+		for i in range(len(theta)-2,-1,-1):
+			D[i] = D[i+1].dot(theta[i+1])
+			D[i] = D[i][:,1:]
+			D[i] = D[i] * self.activationFnGrad(Z[i+1])
 			
-			d2 = np.reshape(d2,(d2.shape[0],1))
-			a1 = np.reshape(a1,(1,a1.shape[0]))
-			
+		G = [d.T.dot(A[i])/m for i,d in enumerate(D)]
 
-			delta2 += d3.dot(a2)
-			delta1 += d2.dot(a1)
-
-		theta1grad = delta1/m
-		theta2grad = delta2/m
-
-		return self.roll([theta1grad,theta2grad])
+		return self.roll(G)
 
 def sigmoid(X):
 	return 1.0 / (1.0 + np.exp(-1. * X))
@@ -179,20 +166,23 @@ def main():
 	y = np.array([i[0] for i in y])
 	data = loadmat('ex3weights.mat')
 	theta = [data['Theta1'],data['Theta2']]
-	N = NeuralNetwork(sigmoid,sigmoidgradient,25,10,X.shape[0],X.shape[1],"ex3test",theta)
+	params = [(25,401),(25,26),(10,26)]
+	N = NeuralNetwork(sigmoid,sigmoidgradient,params,"ex3test",theta)
 	print "Test data accurate to %f" % N.getAccuracy(X,y)
-	print "Cost a test Theta %f" % N.cost_function(X,y,N.roll(theta))
-	print N.gradient(X,y,N.roll(theta))
+	C,G = N.cost_function(X,y,N.roll(theta))
+	print "Cost a test Theta %f" % C
+	print "Gradient: " + str(G)
 
 def prop_test():
 	data = loadmat('ex3data1.mat')
 	X = data['X']
 	y = data['y'] - 1
 	y = np.array([i[0] for i in y])
-	N = NeuralNetwork(sigmoid,sigmoidgradient,25,10,X.shape[0],X.shape[1],"ex3test")
+	params = [(25,401),(10,26),(10,11),(10,11)]
+	N = NeuralNetwork(sigmoid,sigmoidgradient,params,"ex3test")
 	N.train(X,y)
 	print "Test data accurate to %f" % N.getAccuracy(X,y)
 
-if __name__ == "__main__": main()
+if __name__ == "__main__": prop_test()
 
 
